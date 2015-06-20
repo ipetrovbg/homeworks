a0ce4cc8b26d1b2d21a0a1fefe60b41d298367e0
ipetrovbg <ipetrovbg@gmail.com>
2015-06-19 22:50:00 -0700

    edited task6

--- task6.php
+++ task6.php
@@ -1,2 +1,54 @@
 <?php
 
+$game_of_thrones = array(
+    
+    'character1'    => 'Tyrion Lannister',
+    'character2'    => 'Daenerys Targaryen',
+    'character3'    => 'Jon Snow',
+    'character4'    => 'Arya Stark',
+    'character5'    => 'Bran Stark'
+);
+
+$titanic = array(
+    
+    'character1'   => 'Jack Dawson',
+    'character2'   => 'Rose DeWitt Bukater',
+    'character3'   => 'Caledon Nathan',
+    'character4'   => 'Ruth DeWitt Bukater',
+    'character5'   => 'Rose Dawson Calvert',
+);
+
+$The_Bridges_of_Madison_County = array(
+    
+    'character1'    => 'Robert Kincaid',
+    'character2'    => 'Francesca Johnson',
+    'character3'    => 'Carolyn Johnson',
+    'character4'    => 'Michael Johnson',
+    'character5'    => 'Madge',
+    
+);
+
+$harry_potter = array(
+    
+    'character1'    => 'Harry Potter',
+    'character2'    => 'Ron Weasley',
+    'character3'    => 'Hermione Granger',
+    'character4'    => 'Albus Dumbledore',
+    'character5'    => 'Severus Snape',
+);
+
+$chef = array(
+    
+    'character1'    => 'Carl Casper',    
+    'character2'    => 'Inez',    
+    'character3'    => 'Martin',    
+    'character4'    => 'Molly',    
+    'character5'    => 'Ramsey Michel',    
+);
+
+echo '$The_Bridges_of_Madison_County<br />';
+print_r($The_Bridges_of_Madison_County);
+
+echo '<hr />';
+echo '$harry_potter<br />';
+print_r($harry_potter);
